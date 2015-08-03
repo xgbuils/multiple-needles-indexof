d6bedaaec8ca7a396a9076550fc08ea215d3fda2
Xavier Garcia Buils <xgbuils@gmail.com>
2015-08-03 22:42:42 +0200

    change name of package

--- test/test.js
+++ test/test.js
@@ -2,21 +2,21 @@ var chai = require('chai')
 var expect = chai.expect
 chai.should()
 
-var mmIndexOf = require('../src/index.js')
+var mnIndexOf = require('../src/index.js')
 
 describe('multi-match-indexof', function () {
     it('str.indexOf(m, os) === multi-match-indexof(str, m, os).index', function () {
         var str = 'aaabccc'
         var m = 'bc'
         var os = 1
-        mmIndexOf(str, m, os).index.should.be.equal(str.indexOf(m, os))
+        mnIndexOf(str, m, os).index.should.be.equal(str.indexOf(m, os))
     })
 
     it('with multiple needles returns first occurence of string', function () {
         var str = 'ababccc'
         var m = ['c', 'a']
         var os = 1
-        mmIndexOf(str, m, os).should.be.deep.equal({
+        mnIndexOf(str, m, os).should.be.deep.equal({
             index: 2,
             match: 'a'
         })
@@ -25,7 +25,7 @@ describe('multi-match-indexof', function () {
     it('offset parameter is by default 0', function () {
         var str = 'ababccc'
         var m = ['c', 'a']
-        mmIndexOf(str, m).should.be.deep.equal({
+        mnIndexOf(str, m).should.be.deep.equal({
             index: 0,
             match: 'a'
         })
@@ -34,14 +34,14 @@ describe('multi-match-indexof', function () {
     it('if is passed empty array of needles returns undefined', function () {
         var str = 'ababccc'
         var m = []
-        expect(mmIndexOf(str, m)).to.equal(undefined)
+        expect(mnIndexOf(str, m)).to.equal(undefined)
     })
 
     it('if needles contains empty string always matches', function () {
         var str = 'ababccc'
         var m = ['']
         var os = 2
-        mmIndexOf(str, m, os).should.be.deep.equal({
+        mnIndexOf(str, m, os).should.be.deep.equal({
             index: os,
             match: ''
         })
